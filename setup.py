130d59a085e2f54e37ad66b245c92046b546006a
Matic Perovšek <mperice@gmail.com>
2017-12-20 14:04:40 +0100

    examples moved to folder setup.py updated

--- setup.py
+++ setup.py
@@ -10,12 +10,16 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='reldi',
     version='1.7',
-    packages=['rdm'],
+    packages=['reldi'],
+    py_modules=['reldi'],
     include_package_data=True,
     license='Apache License 2.0',
-    description='Relational data mining in python',
+    description='Python library for the ReLDI API',
     short_desciption="Reldi library",
     long_description=README,
     url='https://github.com/clarinsi/reldi-lib',
     author='Filip Petkovski',
+    author_email='filip.petkovsky@gmail.com',
+    keywords=['reldi', 'linguistics'],  # arbitrary keywords
+    classifiers=[],
 )
